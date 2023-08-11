d38d6f87ef21158f284f126c4ddc4a29872118d2
TalelMejri <talelmejri8@gmail.com>
2023-08-11 16:21:23 +0100

    route between first page and second page

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:navigator_push_pop/SecondPage.dart';
 
 
 class FirstPage extends StatelessWidget{
-  FirstPage({super.key,required this.title});
+  const FirstPage({super.key,required this.title});
   final String title;
   @override
   Widget build(BuildContext context){
@@ -10,7 +11,25 @@ class FirstPage extends StatelessWidget{
       appBar: AppBar(
         title: Text("firstPage"),
       ),
-      body: Center(child: Text("hdhd")),
+      body:  Center(child: 
+          Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+                    Text("First Screen",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 48,color: Colors.teal),
+                    ),
+                    const Padding(padding: EdgeInsets.only(bottom: 20)),
+                    ElevatedButton(
+                      onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>SecondPage(title: "ddd")));
+                      },
+                          style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.teal, 
+                        ),
+                     child: Text("Next Screen"),
+                    )
+      ],
+      ),
+      ),
     );
   }
 }
---
+++ lib/SecondPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:navigator_push_pop/FirstPage.dart';
+
+
+class SecondPage extends StatelessWidget{
+  const SecondPage({super.key,required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+       appBar: AppBar(
+        title: Text("SecondPage"),  
+      ),
+      body: Center(child: Column( 
+         mainAxisAlignment: MainAxisAlignment.center,
+         children:<Widget> [
+            Text("SECOND PAGE",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 48,color: Colors.teal)),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context,MaterialPageRoute(builder: (BuildContext context)=>FirstPage(title: "first")));
+            }, child: Text("Previous Page"),style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),)
+         ],
+      ),
+      ),
+    );
+  }
+}
